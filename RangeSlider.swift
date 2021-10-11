fbb9026a005b3c58390f149dde2d9daf8e0eba20
Ryan Rudes <ryanrudes@gmail.com>
2021-10-11 18:31:05 -0400

    Create RangeSlider.swift

---
+++ RangeSlider.swift
@@ -0,0 +1,96 @@
+//
+//  RangeSlider.swift
+//  RangeSlider
+//
+//  Created by Ryan Rudes on 10/11/21.
+//
+
+import SwiftUI
+
+struct RangeSlider<V>: View where V : BinaryFloatingPoint, V.Stride : BinaryFloatingPoint {
+    @Binding var lo: V
+    @Binding var hi: V
+    var bounds: ClosedRange<V>
+    var onEditingChanged: (Bool) -> Void
+    
+    init(lo: Binding<V>, hi: Binding<V>, in bounds: ClosedRange<V> = 0...1, onEditingChanged: @escaping (Bool) -> Void = { _ in }) {
+        self._lo = lo
+        self._hi = hi
+        self.bounds = bounds
+        self.onEditingChanged = onEditingChanged
+    }
+    
+    init(lo: Binding<V>, hi: Binding<V>, in bounds: ClosedRange<V>, step: V.Stride = 1, onEditingChanged: @escaping (Bool) -> Void = { _ in }) where V : BinaryFloatingPoint, V.Stride : BinaryFloatingPoint {
+        self._lo = lo
+        self._hi = hi
+        self.bounds = bounds
+        self.onEditingChanged = onEditingChanged
+    }
+    
+    /// TODO
+    /*
+    init<V>(value: Binding<V>, in bounds: ClosedRange<V> = 0...1, label: () -> Label, onEditingChanged: @escaping (Bool) -> Void = { _ in }) where V : BinaryFloatingPoint, V.Stride : BinaryFloatingPoint {
+        
+    }
+    
+    init<V>(value: Binding<V>, in bounds: ClosedRange<V>, step: V.Stride = 1, label: () -> Label, onEditingChanged: @escaping (Bool) -> Void = { _ in }) where V : BinaryFloatingPoint, V.Stride : BinaryFloatingPoint {
+        
+    }
+    
+    init<V>(value: Binding<V>, in bounds: ClosedRange<V> = 0...1, label: () -> Label, minimumValueLabel: () -> ValueLabel, maximumValueLabel: () -> ValueLabel, onEditingChanged: @escaping (Bool) -> Void = { _ in }) where V : BinaryFloatingPoint, V.Stride : BinaryFloatingPoint {
+        
+    }
+    
+    init<V>(value: Binding<V>, in bounds: ClosedRange<V>, step: V.Stride = 1, label: () -> Label, minimumValueLabel: () -> ValueLabel, maximumValueLabel: () -> ValueLabel, onEditingChanged: @escaping (Bool) -> Void = { _ in }) where V : BinaryFloatingPoint, V.Stride : BinaryFloatingPoint {
+        
+    }
+    */
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 5)
+                    .fill(Color(uiColor: .systemFill))
+                    .frame(width: geometry.size.width, height: 4)
+                
+                RoundedRectangle(cornerRadius: 5)
+                    .fill(.blue)
+                    .frame(width: geometry.size.width * CGFloat(hi - lo) / 100, height: 4)
+                    .offset(x: geometry.size.width * CGFloat(lo) / 100)
+                
+                Circle()
+                    .fill(.white)
+                    .shadow(radius: 1.5, y: 0.75)
+                    .frame(width: 28, height: 28)
+                    .offset(x: geometry.size.width * CGFloat(lo) / 100 - 14)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { gesture in
+                                lo = max(min(V(gesture.location.x / geometry.size.width) * (bounds.upperBound - bounds.lowerBound) + bounds.lowerBound, hi), bounds.lowerBound)
+                                self.onEditingChanged(true)
+                            }
+                            .onEnded { _ in
+                                self.onEditingChanged(false)
+                            }
+                    )
+                
+                Circle()
+                    .fill(.white)
+                    .shadow(radius: 1.5, y: 0.75)
+                    .frame(width: 28, height: 28)
+                    .offset(x: geometry.size.width * CGFloat(hi) / 100 - 14)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { gesture in
+                                hi = min(max(V(gesture.location.x / geometry.size.width) * (bounds.upperBound - bounds.lowerBound) + bounds.lowerBound, lo), bounds.upperBound)
+                                self.onEditingChanged(true)
+                            }
+                            .onEnded { _ in
+                                self.onEditingChanged(false)
+                            }
+                    )
+            }
+        }
+        .padding(.horizontal, 1)
+    }
+}
