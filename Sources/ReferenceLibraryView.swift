8821e152078e386555e2c111bf09aec03429f722
Ryan Rudes <ryanrudes@gmail.com>
2021-11-08 18:52:19 -0500

    Update ReferenceLibraryView.swift

--- Sources/ReferenceLibraryView.swift
+++ Sources/ReferenceLibraryView.swift
@@ -1,5 +1,5 @@
 //
-//  ReferenceLibraryView.swift
+//  ReferenceLibrary.swift
 //  MoreUI
 //
 //  Created by Ryan Rudes on 10/29/21.
@@ -13,16 +13,15 @@ import SwiftUI
  ## Overview
  
  A `UIReferenceLibraryViewController` object should not be used to display wordlists, create a standalone dictionary app, or republish the content in any form.
-
  You create and initialize a reference library view using the init(term:) method. You pass the term to define as the parameter to this method and the definition is displayed. You can present this view modally or as part of another interface. Optionally, use the`dictionaryHasDefinition(forTerm:)` class method to check if a definition is available for a given term before creating an instance—for example, use this method if you want to change the user interface depending on whether a definition is available.
  */
-struct ReferenceLibraryView: UIViewControllerRepresentable {
+public struct ReferenceLibrary: UIViewControllerRepresentable {
     @Environment(\.dismiss) var dismiss
     
-    typealias UIViewControllerType = UIReferenceLibraryViewController
+    public typealias UIViewControllerType = UIReferenceLibraryViewController
     
     /// The term to define.
-    var term: String
+    private var term: String
     
     /**
      Initializes a newly created reference-library view to display the definition of the given term.
@@ -34,12 +33,12 @@ struct ReferenceLibraryView: UIViewControllerRepresentable {
         self.term = term
     }
     
-    func makeUIViewController(context: Context) -> UIViewControllerType {
+    public func makeUIViewController(context: Context) -> UIViewControllerType {
         let controller = UIReferenceLibraryViewController(term: term)
         return controller
     }
 
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    public func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         
     }
 }
@@ -47,6 +46,47 @@ struct ReferenceLibraryView: UIViewControllerRepresentable {
 /// Returns whether a definition is available for the given term.
 /// - Parameter term: The term to be defined.
 /// - Returns: `true` if a definition for `term` is available; otherwise, `false`.
-func dictionaryHasDefinition(forTerm term: String) -> Bool {
+public func dictionaryHasDefinition(forTerm term: String) -> Bool {
     return UIReferenceLibraryViewController.dictionaryHasDefinition(forTerm: term)
+}
+
+public extension View {
+    /**
+     Presents a full-screen reference library when binding to a Boolean value you provide is true.
+     
+     - Parameter term: A binding to a String value that stores the term to define.
+     - Parameter isPresented: A binding to a Boolean value that determines whether to present the sheet.
+     - Parameter onDismiss: The closure to execute when dismissing the modal view.
+     
+     Use this method to show a full-screen reference library. The example below displays a reference library when the user toggles the value of the `isPresenting` binding:
+     
+     ```swift
+     struct ReferenceLibraryPresentedOnDismiss: View {
+        @State private var isPresenting = false
+     
+        var body: some View {
+            TextField("Term", text: $term)
+                .textInputAutocapitalization(.never)
+                .onSubmit {
+                    if !term.isEmpty && dictionaryHasDefinition(forTerm: term) {
+                        isPresented = true
+                    }
+                }
+                .referenceLibrary(term: $term,
+                                  isPresented: $isPresented,
+                                  onDismiss: didDismiss)
+        }
+     
+        func didDismiss() {
+            // Handle the dismissing action.
+        }
+     }
+     ```
+     */
+    func referenceLibrary<Content>(term: Binding<String>, isPresented: Binding<Bool>, onDismiss: (() -> Void)? = nil) -> some View {
+        self
+            .fullScreenCover(isPresented: isPresented, onDismiss: onDismiss) {
+                ReferenceLibrary(term: term.wrappedValue)
+            }
+    }
 }
